4e4c7c4fe61c5cddde138ba3f933435f84f316d0
yiwchen233 <evan9chan@outlook.com>
2023-10-23 12:36:14 -0700

    Add examples

---
+++ example_underquad.py
@@ -0,0 +1,14 @@
+import numpy as np
+import QARSTA
+
+def testfnc(x):
+    return x[0]**2 + x[1]**2
+
+def testfnc_res(x):
+    return x
+
+x0 = np.array([1.0, 1.0])
+
+np.random.seed(0)
+sol = QARSTA.solve(testfnc, x0, p = 2, prand = 1, model_type = "underdetermined quadratic")
+print(sol)
